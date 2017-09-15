7fafdbd1613c879d24885c05be2272a6053c2e11
thinksource <foretribe@gmail.com>
2017-09-16 07:27:14 +1000

    pep8

--- utility.py
+++ utility.py
@@ -28,55 +28,55 @@ def random_date(start, end):
 
 
 def random_temp(lat, conf):
-    temp_conf=conf["latitude_temp"]
-    if(lat>=80):
-        return random.normalvariate(temp_conf["80"][0],temp_conf["80"][1]*0.68)
-    elif(lat<=-80):
-        return random.normalvariate(temp_conf["-80"][0],temp_conf["-80"][1]*0.68)
+    temp_conf = conf["latitude_temp"]
+    if(lat >= 80):
+        return random.normalvariate(temp_conf["80"][0], temp_conf["80"][1]*0.68)
+    elif(lat <= -80):
+        return random.normalvariate(temp_conf["-80"][0], temp_conf["-80"][1]*0.68)
     else:
-        ceil=math.ceil(lat/10)*10
-        floor=math.floor(lat/10)*10
-        mean=(temp_conf[str(ceil)][0]*(ceil-lat)+temp_conf[str(floor)][0]*(lat-floor))/(ceil-floor)
-        sigma=(temp_conf[str(ceil)][1]*(ceil-lat)+temp_conf[str(floor)][1]*(lat-floor))/(ceil-floor)*0.68
+        ceil = math.ceil(lat/10)*10
+        floor = math.floor(lat/10)*10
+        mean = (temp_conf[str(ceil)][0] * (ceil-lat)+temp_conf[str(floor)][0]*(lat-floor))/(ceil-floor)
+        sigma = (temp_conf[str(ceil)][1]*(ceil-lat)+temp_conf[str(floor)][1]*(lat-floor))/(ceil-floor)*0.68
         return random.normalvariate(mean, sigma)
 
 
 def random_pressure(elevation):
-    mid=config.mid_pressure
-    sigma=config.sigma_pressure
+    mid = config.mid_pressure
+    sigma = config.sigma_pressure
     # on average every 9 meters will reduce 100 Pa
-    p=random.normalvariate(mid,sigma)-elevation/9*0.1
+    p = random.normalvariate(mid, sigma) - elevation / 9 * 0.1
     return p
 
 def random_humidity(day):
-    humidity=0
-    if(day=="rain" or day=="snow"):
-        humidity=random.normalvariate(65, 20)
-    elif(day=="cloud"):
-        humidity=random.normalvariate(50,20)
+    humidity = 0
+    if(day == "rain" or day == "snow"):
+        humidity = random.normalvariate(65, 20)
+    elif(day =="cloud"):
+        humidity = random.normalvariate(50,20)
     else:
-        humidity=random.normalvariate(30,20)
-    if humidity<0:
-        humidity=0
+        humidity = random.normalvariate(30,20)
+    if humidity < 0:
+        humidity = 0
     return round(humidity)
 
 
 
 def get_address(lat, lng):
-    args={"latlng":str(lat)+","+str(lng),"key":config.googlekey}
-    url="https://maps.googleapis.com/maps/api/geocode/json?{}".format(urllib.parse.urlencode(args))
+    args = {"latlng":str(lat)+","+str(lng),"key":config.googlekey}
+    url = "https://maps.googleapis.com/maps/api/geocode/json?{}".format(urllib.parse.urlencode(args))
     try:
-        r=requests.get(url)
+        r = requests.get(url)
     except OSError:
         return ""
     rr=""
-    if(r.status_code==200):
-        re=json.loads(r.text)
-        if(re["status"]=="ZERO_RESULTS"):
+    if(r.status_code == 200):
+        re = json.loads(r.text)
+        if(re["status"] =="ZERO_RESULTS"):
             return ""
-        if(len(re["results"])==0):
+        if(len(re["results"]) == 0):
             return ""
-        address=re["results"][0]["address_components"]
+        address = re["results"][0]["address_components"]
         for i in address:
             if "locality" in i["types"] and "political" in i["types"]:
                 if any(char.isdigit() for char in i["long_name"]):
@@ -87,22 +87,22 @@ def get_address(lat, lng):
                     continue
                 return i["short_name"]
             if "administrative_area_level_1" in i["types"]:
-                rr=i["long_name"]
+                rr = i["long_name"]
         return rr
     else:
         return ""
 
 
 def get_height(lat, lng):
-    args={"locations": str(lat)+","+str(lng),"key":config.googlekey}
-    url="https://maps.googleapis.com/maps/api/elevation/json?{}".format(urllib.parse.urlencode(args))
+    args = {"locations": str(lat)+","+str(lng),"key":config.googlekey}
+    url = "https://maps.googleapis.com/maps/api/elevation/json?{}".format(urllib.parse.urlencode(args))
     try:
-        r=requests.get(url)
-        if(r.status_code==200):
-            re=json.loads(r.text)
+        r = requests.get(url)
+        if(r.status_code == 200):
+            re = json.loads(r.text)
             if(len(re["results"])==0):
                 return 0.0
-            d=re["results"][0]
+            d = re["results"][0]
             if int(d['elevation'])<0:
                 return 0
             return d['elevation']
